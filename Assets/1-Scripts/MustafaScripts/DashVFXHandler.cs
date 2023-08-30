c11b9e60768479dce7a398c4eb59fd52ece8bd4a
m-akcay <40623315+m-akcay@users.noreply.github.com>
2023-08-30 17:53:26 +0300

    Dash VFX v0

---
+++ Assets/1-Scripts/MustafaScripts/DashVFXHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DashVFXHandler : MonoBehaviour
+{
+    [SerializeField] private TrailRenderer[] _dashVfx;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(DefaultDashVFXRoutine(0.75f));
+        }
+    }
+
+    private IEnumerator DefaultDashVFXRoutine(float secs)
+    {
+        foreach (var vfx in _dashVfx)
+        {
+            vfx.enabled = true;
+        }
+
+        yield return new WaitForSeconds(secs);
+
+        foreach (var vfx in _dashVfx)
+        {
+            vfx.Clear();
+            vfx.enabled = false;
+        }
+    }
+}
