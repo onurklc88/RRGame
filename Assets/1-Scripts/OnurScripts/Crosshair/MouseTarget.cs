89eff818962f21945d0f9822f244ddb4094e0d17
m-akcay <40623315+m-akcay@users.noreply.github.com>
2023-09-06 17:28:33 +0300

    Cursor rotation test

--- Assets/1-Scripts/OnurScripts/Crosshair/MouseTarget.cs
+++ Assets/1-Scripts/OnurScripts/Crosshair/MouseTarget.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class MouseTarget : MonoBehaviour
 {
-    [SerializeField] private GameObject _depthCamera;
+    [SerializeField] private Camera _depthCamera;
+    [SerializeField] private Camera _mainCamera;
     [SerializeField] private GameObject _character;
     [SerializeField] private GameObject _target;
+
     public LayerMask _targetLayer;
     private LayerMask _groundLayer = 3;
     private int _layerMask;
@@ -33,7 +35,7 @@ public class MouseTarget : MonoBehaviour
     {
         if (_depthCamera == null) return Vector3.zero;
         
-        var ray = _depthCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        var ray = _depthCamera.ScreenPointToRay(Input.mousePosition);
         if (!Physics.Raycast(ray, out var hitInfo, float.MaxValue, _layerMask)) return Vector3.zero;
         var hitPositionIngoredHeight = new Vector3(hitInfo.point.x, _character.transform.position.y, hitInfo.point.z);
         return hitPositionIngoredHeight;
@@ -41,7 +43,16 @@ public class MouseTarget : MonoBehaviour
 
     private Vector3 GetCursorPosition()
     {
-        var ray = _depthCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        var ray = _depthCamera.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out var hitInfo, float.MaxValue, _targetLayer)) return Vector3.zero;
+        var hitPositionIngoredHeight = new Vector3(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
+        return hitPositionIngoredHeight;
+    }
+
+    private Vector3 GetCharacterPosition()
+    {
+        var charScreenPos = _mainCamera.WorldToScreenPoint(_character.transform.position);
+        var ray = _depthCamera.ScreenPointToRay(charScreenPos);
         if (!Physics.Raycast(ray, out var hitInfo, float.MaxValue, _targetLayer)) return Vector3.zero;
         var hitPositionIngoredHeight = new Vector3(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
         return hitPositionIngoredHeight;
@@ -50,6 +61,9 @@ public class MouseTarget : MonoBehaviour
     private void CursorMovement()
     {
         _target.transform.position = GetCursorPosition();
+        var charPos = GetCharacterPosition();
+        var lookAtVector = _target.transform.position - charPos;
+        _target.transform.LookAt(_target.transform.position + lookAtVector);
     }
 
     private void OnDrawGizmos()
